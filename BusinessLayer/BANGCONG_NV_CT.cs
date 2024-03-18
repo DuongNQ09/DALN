4456aedab594c9ca801307371004cded0453be62
Ngo Quang Duong <96757935+DuongNQ09@users.noreply.github.com>
2024-03-18 11:55:22 +0700

    Add files via upload

---
+++ BusinessLayer/BANGCONG_NV_CT.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class BANGCONG_NV_CT
+    {
+        QUANLYNHANSUEntities db = new QUANLYNHANSUEntities();
+        public tb_BANGCONG_NV_CT getItem(int makycong, int manv, int ngay)
+        {
+            return db.tb_BANGCONG_NV_CT.FirstOrDefault(x=>x.MAKYCONG==makycong && x.MANV==manv && x.NGAY.Value.Day==ngay);
+        }
+        public tb_BANGCONG_NV_CT Add(tb_BANGCONG_NV_CT bcct)
+        {
+            try
+            {
+                db.tb_BANGCONG_NV_CT.Add(bcct);
+                db.SaveChanges();
+                return bcct;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi: " + ex.Message);
+            }
+        }
+        public tb_BANGCONG_NV_CT Update(tb_BANGCONG_NV_CT bcct)
+        {
+            try
+            {
+                tb_BANGCONG_NV_CT bcnv = db.tb_BANGCONG_NV_CT.FirstOrDefault(x=>x.MAKYCONG == bcct.MAKYCONG && x.MANV == bcct.MANV && x.NGAY== bcct.NGAY);
+                bcnv.KYHIEU = bcct.KYHIEU;
+                bcnv.GIOVAO = bcct.GIOVAO;
+                bcnv.GIORA = bcct.GIORA;
+                bcnv.GHICHU = bcct.GHICHU;
+                bcnv.NGAYPHEP = bcct.NGAYPHEP;
+                bcnv.CONGCHUNHAT = bcct.CONGCHUNHAT;
+                bcnv.CONGNGAYLE = bcct.CONGNGAYLE;
+                bcnv.NGAYCONG = bcct.NGAYCONG;
+                bcnv.UPDATED_BY = bcct.UPDATED_BY;
+                bcnv.UPDATED_DATE = bcct.UPDATED_DATE;  
+                db.SaveChanges();
+                return bcct;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi: " + ex.Message);
+            }
+        }
+        public double tongNgayPhep(int makycong, int manv)
+        {
+            return db.tb_BANGCONG_NV_CT.Where(x => x.MAKYCONG == makycong && x.MANV == manv && x.NGAYPHEP!=null).ToList().Sum(p=> p.NGAYPHEP.Value);
+        }
+        public double tongNgayCong(int makycong, int manv)
+        {
+            return db.tb_BANGCONG_NV_CT.Where(x => x.MAKYCONG == makycong && x.MANV == manv && x.NGAYCONG != null).ToList().Sum(p => p.NGAYCONG.Value);
+        }
+    }
+}
---
+++ BusinessLayer/LOAICONG.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class LOAICONG
+    {
+        QUANLYNHANSUEntities db = new QUANLYNHANSUEntities();
+        public tb_LOAICONGG getItem(int idloaicong)
+        {
+            return db.tb_LOAICONGG.FirstOrDefault(x => x.IDLC == idloaicong);
+        }
+        public List<tb_LOAICONGG> getList()
+        {
+            return db.tb_LOAICONGG.ToList();
+        }
+        public tb_LOAICONGG Add(tb_LOAICONGG lc)
+        {
+            try
+            {
+                db.tb_LOAICONGG.Add(lc);
+                db.SaveChanges();
+                return lc;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi: " + ex.Message);
+            }
+        }
+        public tb_LOAICONGG Update(tb_LOAICONGG lc)
+        {
+            try
+            {
+                var _lc = db.tb_LOAICONGG.FirstOrDefault(x => x.IDLC == lc.IDLC);
+                _lc.TENLC = lc.TENLC;
+                _lc.HESO = lc.HESO;
+                _lc.UPDATED_BY = lc.UPDATED_BY;
+                _lc.UPDATED_DATE = lc.UPDATED_DATE;
+                db.SaveChanges();
+                return lc;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi: " + ex.Message);
+            }
+        }
+        public void Delete(int idloaicong, int iduser)
+        {
+            try
+            {
+                var _lc = db.tb_LOAICONGG.FirstOrDefault(x => x.IDLC == idloaicong);
+                _lc.DELETED_BY = iduser;
+                _lc.DELETED_DATE = DateTime.Now;
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi: " + ex.Message);
+            }            
+        }
+    }
+}
